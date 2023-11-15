096e35f7b37d52372e988be64136c0b7b50395ea
Pedro Coelho <coelhopedro152@gmail.com>
2023-11-15 18:11:28 -0300

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Quem foi o primeiro presidente do Brasil?",
+    answer: "Marechal Deodoro da Fonseca",
+    options: [
+      "Getúlio Vargas",
+      "Juscelino Kubitschek",
+      "Marechal Deodoro da Fonseca",
+      "Tancredo Neves"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Qual é o componente mais abundante na atmosfera terrestre?",
+    answer: "Nitrogênio",
+    options: [
+      "Nitrogênio",
+      "Oxigênio",
+      "Dióxido de carbono",
+      "Argônio"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Qual é o maior rio do mundo em termos de volume de água?",
+    answer: "Rio Amazonas",
+    options: [
+      "Rio Nilo",
+      "Rio Amazonas",
+      "Rio Yangtzé",
+      "Rio Mississippi"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Qual é a função principal dos leucócitos no sistema imunológico?",
+    answer: "Combate a infecções",
+    options: [
+      "Transporte de oxigênio",
+      "Digestão de alimentos",
+      "Combate a infecções",
+      "Produção de insulina"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Quem é o autor de Dom Quixote, considerado um dos maiores romances da literatura mundial?",
+    answer: "Miguel de Cervantes",
+    options: [
+      "William Shakespeare",
+      "Miguel de Cervantes",
+      "Jane Austen",
+      " Charles Dickens"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
